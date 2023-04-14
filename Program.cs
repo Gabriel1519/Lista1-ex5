ca8661f0b00d313e9cba077b9278fb7856d3e3e9
Gabriel1519 <130614709+Gabriel1519@users.noreply.github.com>
2023-04-14 19:58:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            double n2;
+            double n3;  
+            double n4;
+            double Media;
+
+            Console.Write("Insira o valor do Primeiro numero: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira o valor do Segundo numero: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira o valor do Terceiro numero: ");
+            n3 = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira o valor do Quarto numero: ");
+            n4 = double.Parse(Console.ReadLine());
+
+            Media = (n1 + n2 + n3 + n4)/4;
+
+            Console.WriteLine("A média aritmética é {0}.", Media);
+        }
+    }
+}
